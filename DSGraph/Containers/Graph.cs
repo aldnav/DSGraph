f268019d3030ef0e8631ce9c2887c68fc320413a
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-04-28 04:41:20 +0800

    Working graph and menu

--- DSGraph/Containers/Graph.cs
+++ DSGraph/Containers/Graph.cs
@@ -1,9 +1,16 @@
 ﻿/**
- * Graph
- * @author: Aldrin A. Navarro
- * CMSC 204 Data Structures and Algorithms
- *
- */
+* Graph
+* @author: Aldrin A. Navarro
+* CMSC 204 Data Structures and Algorithms
+*
+* DFS
+* BFS
+* Search DFS
+* Search BFS
+* 
+*/
+
+using System.Collections;
 
 namespace Containers
 {
@@ -32,40 +39,264 @@ namespace Containers
             Matrix = new int[VERTEX_CAP, VERTEX_CAP];
             VertexCount = 0;
             // init matrix
-            for (int i = 0; i <= VERTEX_CAP; i++)
+            for (int i = 0; i < VERTEX_CAP; i++)
             {
-                for (int j = 0; j <= VERTEX_CAP; j++)
+                for (int j = 0; j < VERTEX_CAP; j++)
                 {
                     Matrix[i, j] = 0;
                 }
             }
         }
 
-        public void AddVertex(string tag)
+        /**
+         * Add a vertex to the set of vertices 
+         */
+        public int AddVertex(string tag)
         {
             Vertices[VertexCount] = new Vertex(tag);
             VertexCount++;
+            return VertexCount - 1;
         }
 
+        // See next 'AddEdge'
         public void AddEdge(int source, int dest)
         {
-            // @NOTE: perhaps move 'Directed' as member
-            // of class Graph?
             AddEdge(source, dest, true);  // default to directed
         }
 
+        /**
+         * Add an edge to the adjacency matrix linking source edge
+         * and destination edge. Supports both directed and undirected
+         * edges.
+         */
         public void AddEdge(int source, int dest, bool directed)
         {
             Matrix[source, dest] = 1;
-            if (!directed)  // if undirected, strict to one cell
+            if (!directed)  // only add symmetry if undirected
                 Matrix[dest, source] = 1;
         }
 
+        /**
+         * Check if a vertex exists in the set of vertices
+         */
+        public int SearchVertex(string tag)
+        {
+            int index = -1;
+            for (int i = 0; i < Vertices.Length; i++)
+            {
+                if (Vertices[i] != null && Vertices[i].Tag == tag)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+
+        /**
+         * Easy helper function instead of doing 
+         *   AddVertex(A)
+         *   AddVertex(B)
+         *   AddEdge(A, B)
+         * 
+         * do this:
+         *   A -> B for directed
+         *   2 <- 6 for directed inverse
+         *   a <-> B  or a - B  for undirected
+         */
+        public void Insert(string A, string Direction, string B)
+        {
+            var Source = A;
+            var Destination = B;
+            bool directed = false;
+            switch (Direction)
+            {
+                case "<-":
+                    Source = A;
+                    Destination = B;
+                    directed = true;
+                    break;
+                case "->":
+                    directed = true;
+                    break;
+                case "<->":  // not required but put anyway
+                case "-":    // for readability
+                default:
+                    break;
+            }
+            int SourceIndex = SearchVertex(Source);
+            if (SourceIndex < 0)
+                SourceIndex = AddVertex(Source);
+            int DestinationIndex = SearchVertex(Destination);
+            if (DestinationIndex < 0)
+                DestinationIndex = AddVertex(Destination);
+            AddEdge(SourceIndex, DestinationIndex, directed);
+        }
+
+        /**
+         * Display the vertex's tag
+         */
         public void DisplayVertex(int vertexID)
         {
             System.Console.Write(Vertices[vertexID].Tag + " ");
         }
 
+        /**
+         * Returns the next unvisited adjacent vertex
+         */
+        private int GetAdjacentUnvisitedVertex(int v)
+        {
+            for (int j = 0; j <= VertexCount - 1; j++)
+                if ((Matrix[v, j] == 1) && (!Vertices[j].IsVisited))
+                    return j;
+            return -1;
+        }
+
+        /**
+         * Reset all vertices as unvisited
+         */
+        public void ResetVisits()
+        {
+            for (int j = 0; j <= VertexCount - 1; j++)
+                Vertices[j].IsVisited = false;
+        }
+
+        /**
+         * Perform a DF Traversal
+         */
+        public void DepthFirstSearchTraverse()
+        {
+            Vertices[0].IsVisited = true;
+            DisplayVertex(0);
+            Stack nodeStack = new Stack();
+            nodeStack.Push(0);  // remember to backtrack here
+            int vNext;
+            while (nodeStack.Count > 0)
+            {
+                vNext = GetAdjacentUnvisitedVertex((int) nodeStack.Peek());
+                if (vNext == -1)
+                    nodeStack.Pop();  // last vertex nowhere to go but back
+                else
+                {
+                    Vertices[vNext].IsVisited = true;
+                    DisplayVertex(vNext);
+                    nodeStack.Push(vNext);  // remember to backtrack here
+                }
+            }
+
+            ResetVisits();
+        }
+
+        /**
+         * Perform a DF Search
+         */
+        public bool DepthFirstSearch(string Key)
+        {
+            bool IsFound = false;
+            Vertices[0].IsVisited = true;
+            DisplayVertex(0);
+            if (Vertices[0].Tag == Key)
+            {
+                IsFound = true;
+                goto Breakdown;
+            }
+
+            Stack nodeStack = new Stack();
+            nodeStack.Push(0);  // remember to backtrack here
+            int vNext;
+            while (nodeStack.Count > 0)
+            {
+                vNext = GetAdjacentUnvisitedVertex((int)nodeStack.Peek());
+                if (vNext == -1)
+                    nodeStack.Pop();  // last vertex nowhere to go but back
+                else
+                {
+                    Vertices[vNext].IsVisited = true;
+                    DisplayVertex(vNext);
+                    nodeStack.Push(vNext);  // remember to backtrack here
+
+                    if (Vertices[vNext].Tag == Key)
+                    {
+                        IsFound = true;
+                        goto Breakdown;
+                    }
+                }
+            }
+            goto Breakdown;
+
+            Breakdown:
+                ResetVisits();
+                return IsFound;
+        }
+
+        /**
+         * Perform a BF Traversal
+         */
+        public void BreadthFirstSearchTraverse()
+        {
+            Queue nodeQueue = new Queue();
+            Vertices[0].IsVisited = true;
+            DisplayVertex(0);
+            nodeQueue.Enqueue(0);
+            int currentVertex, nextVertex;
+            while (nodeQueue.Count > 0)
+            {
+                currentVertex = (int) nodeQueue.Dequeue();
+                nextVertex = GetAdjacentUnvisitedVertex(currentVertex);
+                while (nextVertex != -1) {
+                    Vertices[nextVertex].IsVisited = true;
+                    DisplayVertex(nextVertex);
+                    nodeQueue.Enqueue(nextVertex);
+                    nextVertex = GetAdjacentUnvisitedVertex(currentVertex);
+                }
+            }
+
+            ResetVisits();
+        }
+
+        /**
+         * Perform a BF Search
+         */
+        public bool BreadthFirstSearch(string Key)
+        {
+            bool IsFound = false;
+            Queue nodeQueue = new Queue();
+            Vertices[0].IsVisited = true;
+            DisplayVertex(0);
+            nodeQueue.Enqueue(0);
+
+            if (Vertices[0].Tag == Key)
+            {
+                IsFound = true;
+                goto Breakdown;
+            }
+
+            int currentVertex, nextVertex;
+            while (nodeQueue.Count > 0)
+            {
+                currentVertex = (int)nodeQueue.Dequeue();
+                nextVertex = GetAdjacentUnvisitedVertex(currentVertex);
+                while (nextVertex != -1)
+                {
+                    Vertices[nextVertex].IsVisited = true;
+                    DisplayVertex(nextVertex);
+                    nodeQueue.Enqueue(nextVertex);
+
+                    if (Vertices[nextVertex].Tag == Key)
+                    {
+                        IsFound = true;
+                        goto Breakdown;
+                    }
+
+                    nextVertex = GetAdjacentUnvisitedVertex(currentVertex);
+                }
+            }
+            goto Breakdown;
+
+            Breakdown:
+                ResetVisits();
+                return IsFound;
+        }
 
     }
 
--- DSGraph/Program.cs
+++ DSGraph/Program.cs
@@ -1,15 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/**
+* Graph Program
+* @author: Aldrin A. Navarro
+* CMSC 204 Data Structures and Algorithms
+* 
+*/
+
+
+using Containers;
 
 namespace DSGraph
 {
     class Program
     {
+        static void DisplayMenu()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("╔═══════ Data Structures: Graph ══════╗");
+            System.Console.WriteLine("║ [1] Perform Depth First Traversal   ║");
+            System.Console.WriteLine("║ [2] Perform Breadth First Traversel ║");
+            System.Console.WriteLine("║ [3] Search Graph 1 (DFS)            ║");
+            System.Console.WriteLine("║ [4] Search Graph 2 (BFS)            ║");
+            System.Console.WriteLine("║ [5] Exit                            ║");
+            System.Console.WriteLine("║ See https://i.imgur.com/AYo7QnY.png ║");
+            System.Console.WriteLine("║ for graphs                          ║");
+            System.Console.WriteLine("╚════ (c) 2018 Aldrin Navarro ════════╝");
+            System.Console.Write("Enter option: ");
+        }
+
         static void Main(string[] args)
         {
+            // GRAPH 1
+            Graph graph1 = new Graph();
+            graph1.Insert("1", "->", "2");
+            graph1.Insert("2", "->", "4");
+            graph1.Insert("4", "->", "5");
+            graph1.Insert("5", "->", "2");
+            graph1.Insert("1", "->", "3");
+            graph1.Insert("3", "->", "6");
+            graph1.Insert("6", "->", "4");
+            graph1.Insert("6", "->", "7");
+
+            // GRAPH 2
+            Graph graph2 = new Graph();
+            graph2.Insert("A", "->", "B");
+            graph2.Insert("B", "->", "E");
+            graph2.Insert("E", "->", "C");
+            graph2.Insert("E", "->", "D");
+
+            DisplayMenu();
+            string UserInput = System.Console.ReadLine();
+            while(UserInput != "5")
+            {
+                switch(UserInput)
+                {
+                    case "1": // DFT
+                        System.Console.WriteLine("Perform Depth Fist Traversal");
+                        System.Console.Write("Which graph? [1 or 2]: ");
+                        string GraphInput = System.Console.ReadLine();
+                        Graph selectedGraph = GraphInput == "1" ? graph1 : GraphInput == "2" ?  graph2 : null;
+                        while (selectedGraph == null)
+                        {
+                            System.Console.Write("Which graph? [1 or 2]: ");
+                            GraphInput = System.Console.ReadLine();
+                            selectedGraph = GraphInput == "1" ? graph1 : GraphInput == "2" ? graph2 : null;
+                        }
+                        System.Console.WriteLine("DFS on Graph {0}", GraphInput);
+                        selectedGraph.DepthFirstSearchTraverse();
+                        break;
+
+                    case "2": // BFT
+                        System.Console.WriteLine("Perform Breadth Fist Traversal");
+                        System.Console.Write("Which graph? [1 or 2]: ");
+                        string GraphInput2 = System.Console.ReadLine();
+                        Graph selectedGraph2 = GraphInput2 == "1" ? graph1 : GraphInput2 == "2" ? graph2 : null;
+                        while (selectedGraph2 == null)
+                        {
+                            System.Console.Write("Which graph? [1 or 2]: ");
+                            GraphInput2 = System.Console.ReadLine();
+                            selectedGraph2 = GraphInput2 == "1" ? graph1 : GraphInput2 == "2" ? graph2 : null;
+                        }
+                        System.Console.WriteLine("DFS on Graph {0}", GraphInput2);
+                        selectedGraph2.BreadthFirstSearchTraverse();
+                        break;
+
+                    case "3": // DFS
+                        System.Console.WriteLine("Search Graph 1 (DFS)");
+                        System.Console.Write("Enter search key: ");
+                        string Key = System.Console.ReadLine();
+                        System.Console.Write("Searching ");
+                        if (graph1.DepthFirstSearch(Key))
+                        {
+                            System.Console.WriteLine("...\n'{0}' found in graph!", Key);
+                        } else
+                        {
+                            System.Console.WriteLine("...\n'{0}' not found in graph!", Key);
+                        }
+                        break;
+
+                    case "4": // BFS
+                        System.Console.WriteLine("Search Graph 2 (BFS)");
+                        System.Console.Write("Enter search key: ");
+                        string Key2 = System.Console.ReadLine();
+                        System.Console.Write("Searching ");
+                        if (graph2.DepthFirstSearch(Key2))
+                        {
+                            System.Console.WriteLine("...\n'{0}' found in graph!", Key2);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("...\n'{0}' not found in graph!", Key2);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+                System.Console.WriteLine();
+                DisplayMenu();
+                UserInput = System.Console.ReadLine();
+            }
+            
         }
     }
 }
